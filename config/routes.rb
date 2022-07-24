f171f1e47381d0967bef30fccfec0cb2ff90397f
Alex Clifford <aclifford336@gmail.com>
2022-07-24 14:26:46 +0100

    added light and dark modes and refactored css files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
-
   get 'city_search', to: 'home#search'
+
+  get '/dark_mode', to: 'application#dark_mode', as: 'dark_mode'
+  get '/light_mode', to: 'application#light_mode', as: 'light_mode'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
